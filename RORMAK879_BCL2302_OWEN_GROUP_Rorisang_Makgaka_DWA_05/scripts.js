a50923897f1df2db5a8005b3adae3a116704f400
Rorisang Makgaka <125877628+Matere29@users.noreply.github.com>
2023-06-06 16:15:00 +0200

    Add files via upload

---
+++ RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_DWA_05/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  result.innerText = Math.round(dividend / divider);
+  if(dividend === null && divider === null) {
+    alert('Division not performed. Both values are required in inputs. Try again')
+  }
+});
