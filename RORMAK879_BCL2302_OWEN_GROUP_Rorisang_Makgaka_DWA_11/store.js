fb9b8c4508f5f1b0389578e1992d0f711a5c46b8
Rorisang Makgaka <125877628+Matere29@users.noreply.github.com>
2023-06-27 14:53:31 +0200

    Add files via upload

--- RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_DWA_11/store.js
+++ RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_DWA_11/store.js
@@ -1,49 +1,94 @@
-// Redux-inspired store implementation
+// Define the initial state
+const initialState = {
+  count: 0
+};
+
+// Define action types
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
+// Define action creators
+const increment = () => ({
+  type: INCREMENT
+});
+
+const decrement = () => ({
+  type: DECREMENT
+});
+
+const reset = () => ({
+  type: RESET
+});
+
+// Define reducer function
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      };
+    case RESET:
+      return {
+        ...state,
+        count: 0
+      };
+    default:
+      return state;
+  }
+};
+
+// Create the store
 const createStore = (reducer) => {
-    let state;
-    const listeners = [];
-    const getState = () => state;
-    const dispatch = (action) => {
-      state = reducer(state, action);
-      listeners.forEach((listener) => listener());
-    };
-    const subscribe = (listener) => {
-      listeners.push(listener);
-    };
-    // Initialize the store with an initial state
-    dispatch({});
-    return { getState, dispatch, subscribe };
+  let state = reducer(undefined, {});
+  const subscribers = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    subscribers.forEach((subscriber) => subscriber());
   };
-  // Reducer function for the tally counter
-  const tallyReducer = (state = 0, action) => {
-    switch (action.type) {
-      case 'ADD':
-        return state + 1;
-      case 'SUBTRACT':
-        return state - 1;
-      case 'RESET':
-        return 0;
-      default:
-        return state;
-    }
+
+  const subscribe = (subscriber) => {
+    subscribers.push(subscriber);
+    return () => {
+      const index = subscribers.indexOf(subscriber);
+      if (index !== -1) {
+        subscribers.splice(index, 1);
+      }
+    };
   };
-  // Create the store with the tallyReducer
-  const store = createStore(tallyReducer);
-  // Log the state to the console whenever it changes
-  store.subscribe(() => {
-    console.log('State:', store.getState());
-  });
-  // Scenarios
-  // Scenario 1: Increment the counter by one (initial state: 0)
-  console.log('Scenario 1');
-  console.log('Initial State:', store.getState());
-  // Scenario 2: Increment the counter by one (initial state: 0)
-  console.log('Scenario 2');
-  store.dispatch({ type: 'ADD' });
-  store.dispatch({ type: 'ADD' });
-  // Scenario 3: Decrement the counter by one (initial state: 2)
-  console.log('Scenario 3');
-  store.dispatch({ type: 'SUBTRACT' });
-  // Scenario 4: Resetting the Tally Counter (initial state: 1)
-  console.log('Scenario 4');
-  store.dispatch({ type: 'RESET' });
+
+  return {
+    getState,
+    dispatch,
+    subscribe
+  };
+};
+
+// Usage
+
+// Create the store with the reducer
+const store = createStore(reducer);
+
+// Subscribe to the store updates
+const unsubscribe = store.subscribe(() => {
+  const state = store.getState();
+  console.log("Current count:", state.count);
+});
+
+// Dispatch actions to update the state
+store.dispatch(increment()); // Current count: 1
+store.dispatch(increment()); // Current count: 2
+store.dispatch(decrement()); // Current count: 1
+store.dispatch(reset());    // Current count: 0
+
+// Unsubscribe from the store updates
+unsubscribe();
